9f681cef3a89512336b91d49f4a77529203c366e
Naveen Varshney <naveen.varshney29@gmail.com>
2018-02-18 00:04:57 +0530

    setting for static

--- hackpy/settings.py
+++ hackpy/settings.py
@@ -16,7 +16,6 @@ from decouple import config, Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR,"templates")
-STATIC_DIR = os.path.join(BASE_DIR,"static")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
