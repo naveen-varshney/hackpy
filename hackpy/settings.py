7fdbf9244bdd7f58bb7a06e7cd753257dac1760a
Naveen Varshney <naveen.varshney29@gmail.com>
2018-02-17 23:33:29 +0530

    set env file

--- hackpy/settings.py
+++ hackpy/settings.py
@@ -30,6 +30,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 ALLOWED_HOSTS = []
 
 
@@ -83,10 +87,10 @@ WSGI_APPLICATION = 'hackpy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hackpy_db',
-        'USER': 'hackpy_user',
-        'PASSWORD': 'hackpy_password',
-        'HOST': 'localhost',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '',
     }
 }
